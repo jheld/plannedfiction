2c04d378198862b7ba55e7a23c402d3d23534667
Jason Held <jsonheld@gmail.com>
2013-01-07 21:10:19 -0500

    Add a generic text-area and fit it to a somewhat idealistic size.

--- fictionProject/piece/admin.py
+++ fictionProject/piece/admin.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from models import Piece, Character, Event#, Membership
+from models import Piece
 
 class PieceAdmin(admin.ModelAdmin):
     pass
 
-class CharacterAdmin(admin.ModelAdmin):
-    pass
-class EventAdmin(admin.ModelAdmin):
-    pass
-'''
-class MembershipAdmin(admin.ModelAdmin):
-    pass
-'''
-
 admin.site.register(Piece, PieceAdmin)
-admin.site.register(Character, CharacterAdmin)
-admin.site.register(Event, EventAdmin)
-# admin.site.register(Membership, MembershipAdmin)
